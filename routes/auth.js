50fb2bebba2ac32f2da72991e7936214179dc43c
Albert Sare <thesundaymailman@gmail.com>
2018-06-12 11:39:17 -0700

    updated '/login' endpoint to return jwt upon successful login

--- routes/auth.js
+++ routes/auth.js
@@ -2,15 +2,27 @@
 
 const express = require('express');
 const passport = require('passport');
+const jwt = require('jsonwebtoken');
+
+const { JWT_SECRET, JWT_EXPIRY } = require('../config.js');
 
 const options = {session: false, failWithError: true};
 const localAuth = passport.authenticate('local', options);
 
 const router = express.Router();
 
+// accepts user object and creates jwt token
+const createAuthToken = function(user) {
+  return jwt.sign({ user }, JWT_SECRET, {
+    subject: user.username,
+    expiresIn: JWT_EXPIRY
+  });
+};
+
 // POST
 router.post('/login', localAuth, (req, res) => {
-  return res.json(req.user);
+  const authToken = createAuthToken(req.user);
+  res.json({ authToken });
 });
 
 module.exports = router;
